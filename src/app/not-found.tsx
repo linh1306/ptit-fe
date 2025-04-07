8bc6709f67de4318ca393fa78f8ddcd994eed2c6
linh.nv <linh.nv@tinasoft.com.vn>
2025-04-07 16:13:35 +0700

    feat: page 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,24 @@
-export default function Custom404() {
-    return <h1>404 - Trang không tìm thấy</h1>
-  }
-  
-  // Nếu dùng App Router
-  export const runtime = 'edge';  // Thử chạy trên Edge Runtime
+import React from 'react';
+import { Button, Result } from 'antd';
+import Link from 'next/link';
+
+const NotFoundPage = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <Result
+        status="404"
+        title="404"
+        subTitle="Xin lỗi, trang bạn tìm kiếm không tồn tại."
+        extra={
+          <Link href="/">
+            <Button type="primary" className="bg-blue-500 hover:bg-blue-600">
+              Quay lại trang chủ
+            </Button>
+          </Link>
+        }
+      />
+    </div>
+  );
+};
+
+export default NotFoundPage;
